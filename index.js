7bdf2784d34b117cfc3c8fb1c96e75d3928a8333
Bart Evers <bart@gillz.nl>
2020-05-19 14:18:30 +0200

    Calculations BMI

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+function calculateBMI(weight, height) {
+  // we can just return the expressions
+  // That way we don't need another variable
+  return weight / (height * height);
+}
+
 function bmiCalculator() {
   const weightInKg = parseInt(process.argv[2]);
   const heightInM = parseFloat(process.argv[3]);
@@ -5,11 +11,14 @@ function bmiCalculator() {
   const dailyExercise = process.argv[5];
   const gender = process.argv[6];
 
+  const BMI = calculateBMI(weightInKg, heightInM);
+
   console.log("WEIGHT: ", weightInKg);
   console.log("HEIGHT: ", heightInM);
   console.log("AGE: ", age);
   console.log("DAILY EXERCISE: ", dailyExercise);
   console.log("GENDER: ", gender);
+  console.log("BMI: ", BMI);
 }
 
 bmiCalculator();
