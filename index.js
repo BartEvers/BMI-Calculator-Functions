926c975e3eb2bac16bd0a64320e9f9a72c1d9ac0
Bart Evers <bart@gillz.nl>
2020-05-19 14:16:53 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-function calculateBMI(weight, height) {
-  return weight / (height * height);
-}
-
-function calculateBMR(weight, height, ageOfUser, genderOfUser) {
-  const heightInCm = height * 100;
-
-  let BMR;
-
-  if (genderOfUser === "m") {
-    BMR = 10 * weight + 6.25 * heightInCm - 5 * ageOfUser + 50;
-  } else {
-    BMR = 10 * weight + 6.25 * heightInCm - 5 * ageOfUser - 150;
-  }
-
-  return BMR;
-}
-
 function bmiCalculator() {
   const weightInKg = parseInt(process.argv[2]);
   const heightInM = parseFloat(process.argv[3]);
@@ -23,16 +5,11 @@ function bmiCalculator() {
   const dailyExercise = process.argv[5];
   const gender = process.argv[6];
 
-  const BMI = calculateBMI(weightInKg, heightInM);
-  const BMR = calculateBMR(weightInKg, heightInM, age, gender);
-
   console.log("WEIGHT: ", weightInKg);
   console.log("HEIGHT: ", heightInM);
   console.log("AGE: ", age);
   console.log("DAILY EXERCISE: ", dailyExercise);
   console.log("GENDER: ", gender);
-  console.log("BMI: ", BMI);
-  console.log("BMR: ", BMR);
 }
 
 bmiCalculator();
