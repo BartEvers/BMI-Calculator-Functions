dedab7d19f7ae2ad7183091c455cd2d53f147160
Bart Evers <bart@gillz.nl>
2020-05-19 13:31:49 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function bmiCalculator() {
+  const weightInKg = parseInt(process.argv[2]);
+  const heightInM = parseFloat(process.argv[3]);
+  const age = parseInt(process.argv[4]);
+  const dailyExercise = process.argv[5];
+  const gender = process.argv[6];
+
+  console.log("WEIGHT: ", weightInKg);
+  console.log("HEIGHT: ", heightInM);
+  console.log("AGE: ", age);
+  console.log("DAILY EXERCISE: ", dailyExercise);
+  console.log("GENDER: ", gender);
+}
+
+bmiCalculator();
