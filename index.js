093fb31de3b78063eadfef1d82b75131d0864c65
Bart Evers <bart@gillz.nl>
2020-05-19 15:11:19 +0200

    Validation Weight Height Age

--- index.js
+++ index.js
@@ -34,12 +34,78 @@ function calculateDietCalories(weightToLose, caloriesUsedDaily) {
   return weightToLose > 0 ? caloriesUsedDaily - 500 : caloriesUsedDaily + 500;
 }
 
+function validateNumberOfInputs(argv) {
+  if (argv.length !== 7) {
+    console.log(`
+      You gave ${argv.length - 2} arguments(s) to the program
+  
+      Please provide 5 arguments for
+      
+      weight (kg), 
+      height (m), 
+      age (years), 
+      wether you exercise daily (yes or no)
+      and your gender (m or f)
+      
+      Example:
+  
+      $ node index.js 82 1.79 32 yes m
+    `);
+
+    process.exit();
+  }
+}
+
+function validateWeightHeightAndAge(weight, height, ageOfUser, argv) {
+  if (isNaN(weight) || isNaN(height) || isNaN(ageOfUser)) {
+    console.log(`
+      Please make sure weight, height and age are numbers:
+      weight (kg) example: 82 | your input: ${argv[2]}
+      height (m) example 1.79 | your input: ${argv[3]}
+      age (years) example 32  | your input: ${argv[4]} 
+      $ node index.js 82 1.79 32 yes m
+    `);
+
+    process.exit();
+  }
+
+  if (ageOfUser < 20) {
+    console.log(`
+      This BMI calculator was designed to be used by people older than 20
+  
+      BMI is calculated differently for young people.
+  
+      Please visit: https://en.wikipedia.org/wiki/Body_mass_index#Children_(aged_2_to_20)
+  
+      For more information
+    `);
+
+    process.exit();
+  }
+
+  if (weight < 30 || weight > 300) {
+    console.log(`
+      Please enter a weight in kgs
+      
+      Your weight of ${weight} kgs does not fall in the range between 30 kg and 300 kg
+  
+      If you weight is below 30 kg or over 300 kg seek professional medical help
+    `);
+
+    process.exit();
+  }
+}
+
 function bmiCalculator() {
+  validateNumberOfInputs(process.argv);
+
   const weightInKg = parseInt(process.argv[2]);
   const heightInM = parseFloat(process.argv[3]);
   const age = parseInt(process.argv[4]);
   const dailyExercise = process.argv[5];
   const gender = process.argv[6];
+
+  validateWeightHeightAndAge(weightInKg, heightInM, age, process.argv);
 
   const BMI = calculateBMI(weightInKg, heightInM);
   const idealWeightKg = calculateIdealWeight(heightInM);
@@ -48,19 +114,6 @@ function bmiCalculator() {
   const weightToLoseKg = weightInKg - idealWeightKg;
   const dietWeeks = calculateDietWeeks(weightToLoseKg);
   const dietCalories = calculateDietCalories(weightToLoseKg, dailyCalories);
-
-  console.log("DIET CALORIES:", dietCalories);
-  console.log("WEIGHT: ", weightInKg);
-  console.log("HEIGHT: ", heightInM);
-  console.log("AGE: ", age);
-  console.log("DAILY EXERCISE: ", dailyExercise);
-  console.log("GENDER: ", gender);
-  console.log("BMI: ", BMI);
-  console.log("IDEAL WEIGHT", idealWeightKg);
-  console.log("BMR: ", BMR);
-  console.log("DAILYCALORIES:", dailyCalories);
-  console.log("WEIGHT TO LOSE", weightToLoseKg);
-  console.log("DIETWEEKS", dietWeeks);
 }
 
 bmiCalculator();
